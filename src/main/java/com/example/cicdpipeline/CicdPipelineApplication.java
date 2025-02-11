ee408f983f7df6d7a8bb7b9a176e83067688993f
Sachin Tandan <sachin8peace@gmail.com>
2025-02-11 16:08:48 -0600

    initial commit

---
+++ src/main/java/com/example/cicdpipeline/CicdPipelineApplication.java
@@ -0,0 +1,20 @@
+package com.example.cicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class CicdPipelineApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        SpringApplication.run(CicdPipelineApplication.class, args);
+
+
+    }
+}
---
+++ src/main/java/com/example/cicdpipeline/NameController.java
@@ -0,0 +1,15 @@
+package com.example.cicdpipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NameController {
+
+    @GetMapping
+    public String homePage() {
+        return "Welcome to the Home Page!";
+    }
+}
